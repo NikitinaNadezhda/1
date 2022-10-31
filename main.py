cf37010d684d5ec08dfb18e8f15384751ac65c7f
NadiaNikitina <fyywq@mailto.plus>
2022-10-31 21:12:53 +0300

    fin

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-try:
-    f1=open('data.txt','r')
-    data1=['31','12','1900']
-    while True:
-        data = f1.readline().split("-")
-        print(data)
-        if data[0] == '':
-            break
-        if int(data[0]) < int(data1[0]):
-            data1[0] = data[0]
-        if int(data[1]) < int(data1[1]):
-            data1[1] = data[1]
+def openFile():
+    try:
+        f1=open('data.txt', 'r')
+        while True:
+            data = f1.readline().split("-")
+            print(data)
+            if data[0] =='':
+                break
+            return data
+    except IOError:
+        print(IOError.with_traceback())
 
-    f1.close()
+def dateMoth():
+    data2 = openFile()
+    print(data2)
+    data1=['31','12','1980']
+    if int(data2[0]) <int(data1[0]):
+        data1[0] =data2[0]
+    if int(data2[1]) <int(data1[1]):
+        data1[1] = data2[1]
     print(data1[0], "-", data1[1])
-except IOError:
-    print(IOError.with_traceback())
-
